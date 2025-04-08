f0884d3ca2506d47ab6c02c95e50a4b8155408dd
cmd-AJ <andrewlol774@gmail.com>
2025-04-08 06:55:25 -0500

    Adding to Server

--- ConsortBack/ConsortBack/settings.py
+++ ConsortBack/ConsortBack/settings.py
@@ -64,6 +64,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'ConsortBack.urls'
@@ -148,9 +149,29 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #Permite or allows to origins from any other domains such as my frontend
 CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_HEADERS = [
-    'content-type',
-    'authorization',
-    'x-csrftoken',
 
+CORS_ALLOW_METHODS = [
+    'GET',
+    'POST',
+    'PUT',
+    'PATCH',
+    'DELETE',
+    'OPTIONS',
 ]
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8000',
+] 
